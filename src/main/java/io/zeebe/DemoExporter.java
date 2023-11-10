903c5a3a70d40543e19b070dad3a6fd368835de7
McAlm <stefan.wiese@camunda.com>
2023-11-10 12:14:12 +0100

    upgraded to 8.3.1

--- src/main/java/io/zeebe/DemoExporter.java
+++ src/main/java/io/zeebe/DemoExporter.java
@@ -1,14 +1,20 @@
 package io.zeebe;
 
-import io.zeebe.exporter.api.context.Context;
-import io.zeebe.exporter.api.context.Controller;
-import io.zeebe.exporter.api.Exporter;
-import io.zeebe.protocol.record.Record;
+import io.camunda.zeebe.exporter.api.Exporter;
+import io.camunda.zeebe.exporter.api.context.Context;
+import io.camunda.zeebe.exporter.api.context.Controller;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.RecordType;
+import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent;
+import io.camunda.zeebe.protocol.record.value.BpmnElementType;
+import io.camunda.zeebe.protocol.record.value.ProcessInstanceRecordValue;
 
 public class DemoExporter implements Exporter {
     Controller controller;
 
     public void configure(Context context) throws Exception {
+        context.getConfiguration();
     }
 
     public void open(Controller controller) {
@@ -18,8 +24,35 @@ public class DemoExporter implements Exporter {
     public void close() {
     }
 
-    public void export(Record record) {
-        System.out.println(record.toJson());
-        this.controller.updateLastExportedRecordPosition(record.getPosition());
+    public void export(final Record<?> record) {
+
+        try {
+
+            if (isEventSubprocessEnded(record)) {
+                System.out.println(" ######  Event Subprocess ended! "
+                        + ((ProcessInstanceRecordValue) record.getValue()).getElementId() + " ###### ");
+
+                System.out.println("RecordType: " + record.getRecordType());
+                System.out.println("ValueType: " + record.getValueType());
+                System.out.println("Intent: " + record.getIntent());
+                System.out.println("Value " + record.getValue());
+                System.out.println("Value Class " + record.getValue().getClass().getName());
+            }
+
+        } finally {
+            this.controller.updateLastExportedRecordPosition(record.getPosition());
+        }
+
+    }
+
+    private Boolean isEventSubprocessEnded(final Record<?> record) {
+        if (!(record.getValue() instanceof ProcessInstanceRecordValue)) {
+            return false;
+        }
+        return record.getRecordType() == RecordType.EVENT &&
+                record.getValueType() == ValueType.PROCESS_INSTANCE &&
+                record.getIntent() == ProcessInstanceIntent.ELEMENT_COMPLETED &&
+                ((ProcessInstanceRecordValue) record.getValue())
+                        .getBpmnElementType() == BpmnElementType.EVENT_SUB_PROCESS;
     }
 }
